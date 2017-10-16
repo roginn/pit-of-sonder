d68ef779ba5fd12ea8878ad8a0db0e8d62bc601b
Roger Garcia <roger@quero.education>
2017-10-16 01:05:12 -0200

    Now we can start a game

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,4 +1,4 @@
-class MatchesController < ApplicationController
+  class MatchesController < ApplicationController
   def new_game
     match = Match.find_or_create_by(started: false)
     response = {
@@ -11,13 +11,13 @@ class MatchesController < ApplicationController
 
   def join_game
     bot_name = params['bot_name'].to_s
-    unless bot_name.present?
+    if bot_name.blank?
       return render json: { success: false, error: "Invalid bot name: #{bot_name}" }
     end
 
     game_id = params[:game_id].to_i
-    match = Match.find_by(id: game_id)
-    unless match.present?
+    match = Match.find(game_id)
+    if match.blank?
       return render json: { success: false, error: "Match not found" }
     end
 
@@ -31,9 +31,24 @@ class MatchesController < ApplicationController
 
   def get_lobby
     @match = Match.first_not_started
+
+    binding.pry
+
+    if @match.blank?
+      return render inline: "Nobody waiting for a game to start!"
+    end
+
     render "index"
   end
 
   def post_lobby
+    match_id = params['game_id'].to_i
+    @match = Match.find(match_id)
+    unless @match.present?
+      return render inline: "Match_id #{match_id} not found"
+    end
+
+    @match.start!
+    render "started"
   end
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -3,4 +3,9 @@ class Match < ApplicationRecord
   has_many :players
 
   scope :first_not_started, -> { where(started: false).first }
+
+  def start!
+    self.started = true
+    save
+  end
 end
